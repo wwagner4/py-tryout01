0230f9063f410895f7a3dad4e4aa64719aa3eda0
wwagner4 <wwagner4@gmail.com>
2018-05-08 20:46:24 +0200

    h5, sclicing

--- slicing.py
+++ slicing.py
@@ -1,5 +1,19 @@
 import numpy as np
 
-x = np.random.random()
-print(x)
+a = np.array([
+    np.arange(1, 5),
+    np.arange(1, 5),
+    np.arange(1, 5),
+    np.arange(1, 5),
+    np.arange(1, 5),
+    np.arange(1, 5)
+])
 
+x = a[:, :-1]
+y = a[:, -1:]
+
+print("a {}".format(a.shape))
+print("x {}".format(x.shape))
+print("y {}".format(y.shape))
+print("x {}".format(x))
+print("y {}".format(y))
